0bb4e23fafa56e9e998dd29d360c5dac9616f6c2
emilytowery <57596142+emilytowery@users.noreply.github.com>
2022-04-13 10:16:49 -0400

    Set up the rest of the different activities/buttons to navigate through them

--- app/src/main/java/edu/uga/cs/bankstoweryfinalproject/HomeActivity.java
+++ app/src/main/java/edu/uga/cs/bankstoweryfinalproject/HomeActivity.java
@@ -2,13 +2,87 @@ package edu.uga.cs.bankstoweryfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button grocery;
+    private Button recentPurchases;
+    private Button money;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        grocery = findViewById(R.id.groceryButton);
+        recentPurchases = findViewById(R.id.recentPurchaseButton);
+        money = findViewById(R.id.moneyButton);
+
+        grocery.setOnClickListener(new GroceryButtonClickListener());
+        recentPurchases.setOnClickListener(new RecentButtonClickListener());
+        money.setOnClickListener(new MoneyButtonClickListener());
+
+
+    }
+    /**
+     * Button that lets the users take a quiz.
+     */
+    private class RecentButtonClickListener implements
+            View.OnClickListener {
+        /**
+         * onCLick function for our overview button
+         * takes us to the overview page, passing along the spinner selection
+         *
+         * @param view
+         */
+        @Override
+        public void onClick(View view) {
+            Intent intent = new
+                    Intent(view.getContext(),
+                    RecentPurchaseActivity.class);
+            startActivity(intent);
+        }
+    }
+    /**
+     * Button that lets the users take a quiz.
+     */
+    private class GroceryButtonClickListener implements
+            View.OnClickListener {
+        /**
+         * onCLick function for our overview button
+         * takes us to the overview page, passing along the spinner selection
+         *
+         * @param view
+         */
+        @Override
+        public void onClick(View view) {
+            Intent intent = new
+                    Intent(view.getContext(),
+                    GroceryListActivity.class);
+            startActivity(intent);
+        }
+    }
+    /**
+     * Button that lets the users take a quiz.
+     */
+    private class MoneyButtonClickListener implements
+            View.OnClickListener {
+        /**
+         * onCLick function for our overview button
+         * takes us to the overview page, passing along the spinner selection
+         *
+         * @param view
+         */
+        @Override
+        public void onClick(View view) {
+            Intent intent = new
+                    Intent(view.getContext(),
+                    MoneyActivity.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/edu/uga/cs/bankstoweryfinalproject/MoneyActivity.java
@@ -0,0 +1,42 @@
+package edu.uga.cs.bankstoweryfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MoneyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_money);
+
+
+        //action bar to enable to back button
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    /**
+     * takes in the item and uses it to navigate back to the previous page
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+            supportNavigateUpTo(new Intent(this, HomeActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
